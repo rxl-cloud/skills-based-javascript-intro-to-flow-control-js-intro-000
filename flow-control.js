64b4b721ea4ba566db2b36cb14716e003547c76c
Ruoxin Lin <ruoxinlin.ltg@gmail.com>
2019-07-23 03:31:56 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,9 +28,7 @@ function ternaryTeenager(age) {
  if(age >= 13 && age <= 19) {
    return"You are a teenager";
 }
-   else {
-     return"You are not a teenager";
-}
+   else {return"You are not a teenager";}
 }
 
 function switchAge(age) {
