a01af4b42705d0d80d8acc6c00a41a6563ce9a38
Ruoxin Lin <ruoxinlin.ltg@gmail.com>
2019-07-23 03:37:00 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -18,8 +18,8 @@ function ageChecker(age) {
     else if(age <= 12) {
       return"You are a kid";
 }
-    else if(age > 19) {
-      return"You are a grownup";
+    else{return "You are a grownup";
+}
 }
 
 function ternaryTeenager(age) {
